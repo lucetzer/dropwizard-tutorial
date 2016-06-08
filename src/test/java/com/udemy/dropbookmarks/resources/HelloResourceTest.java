e7731f705504b6db2adf4daf7af7ddf9fc730bca
Lucy Fang <lucyfang2@gmail.com>
2016-06-08 17:22:09 +0100

    add test for password secured resource

--- src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
+++ src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
@@ -3,7 +3,17 @@ package com.udemy.dropbookmarks.resources;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import com.google.common.base.Optional;
+import com.udemy.dropbookmarks.core.User;
+import io.dropwizard.auth.AuthFactory;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.auth.basic.BasicAuthFactory;
+import io.dropwizard.auth.basic.BasicCredentials;
 import io.dropwizard.testing.junit.ResourceTestRule;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -11,12 +21,37 @@ import static org.junit.Assert.*;
 
 public class HelloResourceTest {
 
+    private static final HttpAuthenticationFeature FEATURE = HttpAuthenticationFeature.basic("u", "p");
+
+    private static final Authenticator<BasicCredentials, User> AUTHENTICATOR
+            = new Authenticator<BasicCredentials, User>() {
+        @Override
+        public Optional<User> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
+            return Optional.of(new User());
+        }
+    };
+
     @ClassRule
     public static final ResourceTestRule RULE
             = ResourceTestRule
             .builder()
+                .addProvider(
+                        AuthFactory.binder(
+                                new BasicAuthFactory<>(
+                                        AUTHENTICATOR,
+                                        "realm",
+                                        User.class
+                                )
+                        )
+                )
+            .setTestContainerFactory(new GrizzlyTestContainerFactory())
             .addResource(new HelloResource())
             .build();
+
+    @BeforeClass
+    public static void setUpClass() {
+        RULE.getJerseyTest().client().register(FEATURE);
+    }
 
     @Test
     public void testGreeting() {
@@ -29,4 +64,14 @@ public class HelloResourceTest {
         assertEquals(expectedMessage, actualMessage);
     }
 
+    @Test
+    public void testGetSecuredGreeting() throws Exception {
+        String expectedMessage = "Hello secured world!";
+        String actualMessage = RULE
+                .getJerseyTest()
+                .target("/hello/secured")
+                .request(MediaType.TEXT_PLAIN)
+                .get(String.class);
+        assertEquals(expectedMessage, actualMessage);
+    }
 }
