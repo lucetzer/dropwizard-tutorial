e41efbbf89af05eb27eba481fce4ca074b409334
Lucy Fang <lucyfang2@gmail.com>
2016-06-08 11:10:31 +0100

    Added secure method to prompt user credentials

--- src/main/java/com/udemy/dropbookmarks/resources/HelloResource.java
+++ src/main/java/com/udemy/dropbookmarks/resources/HelloResource.java
@@ -1,13 +1,25 @@
 package com.udemy.dropbookmarks.resources;
 
+import com.udemy.dropbookmarks.core.User;
+import io.dropwizard.auth.Auth;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class HelloResource {
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
         return "Hello World!";
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/secured")
+    public String getSecuredGreeting(@Auth User user) {
+        return "Hello secured world!";
+    }
+
 }
