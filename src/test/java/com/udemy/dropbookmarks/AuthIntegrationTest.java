973a47201ad0a4b19d7368b024ea2dd57aebf65b
Lucy Fang <lucyfang2@gmail.com>
2016-06-10 17:04:39 +0100

    Added integration test for HTTPS

--- src/test/java/com/udemy/dropbookmarks/AuthIntegrationTest.java
+++ src/test/java/com/udemy/dropbookmarks/AuthIntegrationTest.java
@@ -3,6 +3,8 @@ package com.udemy.dropbookmarks;
 import com.udemy.dropbookmarks.DropBookmarksApplication;
 import com.udemy.dropbookmarks.DropBookmarksConfiguration;
 import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.glassfish.grizzly.ssl.SSLContextConfigurator;
+import org.glassfish.jersey.SslConfigurator;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 import org.junit.After;
@@ -10,6 +12,7 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
@@ -30,13 +33,24 @@ public class AuthIntegrationTest {
 
     private static final String TARGET = "http://localhost:8080";
 
+    private static final String SECURE_TARGET = "https://localhost:8443";
+
     private static final String PATH = "/hello/secured";
+
+    private static final String TRUST_STORE_FILE_NAME = "dropbookmarks.keystore";
+
+    private static final String TRUST_STORE_PASSWORD = "p@ssw0rd";
 
     private Client client;
 
     @Before
     public void setUp() {
-        client = ClientBuilder.newClient();
+        SslConfigurator configurator = SslConfigurator.newInstance();
+        configurator.trustStoreFile(TRUST_STORE_FILE_NAME).trustStorePassword(TRUST_STORE_PASSWORD);
+        SSLContext context = configurator.createSSLContext();
+        client = ClientBuilder.newBuilder()
+                .sslContext(context)
+                .build();
     }
 
     @After
@@ -61,6 +75,30 @@ public class AuthIntegrationTest {
         client.register(FEATURE);
         String actual = client
                 .target(TARGET)
+                .path(PATH)
+                .request()
+                .get(String.class);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSadPathSecure() {
+        Response response = client
+                .target(SECURE_TARGET)
+                .path(PATH)
+                .request()
+                .get();
+
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void testHappyPathSecure() {
+        String expected = "Hello secured world!";
+        client.register(FEATURE);
+        String actual = client
+                .target(SECURE_TARGET)
                 .path(PATH)
                 .request()
                 .get(String.class);
