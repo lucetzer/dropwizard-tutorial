40fc1ddf1ae06069add9a1e1277896566451d0c5
Lucy Fang <lucyfang2@gmail.com>
2016-06-10 17:33:15 +0100

    configured database connection

--- src/main/java/com/udemy/dropbookmarks/DropBookmarksConfiguration.java
+++ src/main/java/com/udemy/dropbookmarks/DropBookmarksConfiguration.java
@@ -2,13 +2,26 @@ package com.udemy.dropbookmarks;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class DropBookmarksConfiguration extends Configuration {
 
     @NotEmpty
     private String password;
+
+    @NotNull
+    @Valid
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
+
+    @JsonProperty("database")
+//    "database" keyword is parsed here and is the same as config.yml
+    private DataSourceFactory dataSourceFactory = new DataSourceFactory();
 
     @JsonProperty
     public String getPassword() {
