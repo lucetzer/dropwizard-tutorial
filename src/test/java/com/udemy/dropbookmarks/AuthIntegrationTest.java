d500c028fd7483cc74fbfb634f1b6ea8a8372f06
Lucy Fang <lucyfang2@gmail.com>
2016-06-10 16:08:20 +0100

    added integration test for resource, application and configuration

---
+++ src/test/java/com/udemy/dropbookmarks/AuthIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.udemy.dropbookmarks;
+
+import com.udemy.dropbookmarks.DropBookmarksApplication;
+import com.udemy.dropbookmarks.DropBookmarksConfiguration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class AuthIntegrationTest {
+
+    private static final String CONFIG_PATH = "config.yml";
+
+    @ClassRule
+    public static final DropwizardAppRule<DropBookmarksConfiguration> RULE
+            = new DropwizardAppRule<> (DropBookmarksApplication.class,
+            CONFIG_PATH);
+
+    private static final String TARGET = "http://localhost:8080";
+
+    private static final String PATH = "/hello/secured";
+
+    private Client client;
+
+    @Before
+    public void setUp() {
+        client = ClientBuilder.newClient();
+    }
+
+    @After
+    public void tearDown() {
+        client.close();
+    }
+
+    @Test
+    public void testSadPath() {
+        Response response = client
+                .target(TARGET)
+                .path(PATH)
+                .request()
+                .get();
+
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
+    }
+
+}
